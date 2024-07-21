3f5fed3a36b1ef5c17574d7befc689661c3cb780
Лузина Мария <kaminskaya.mari@list.ru>
2024-07-21 20:59:05 +0700

    notJSX App.js

---
+++ App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { createElement } from 'react';
+
+export const App = () => {
+	const year = new Date().getFullYear();
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				null,
+				'Edit',
+				createElement('code', null, 'src/App.js'),
+				'and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('div', null, year),
+		),
+	);
+};
